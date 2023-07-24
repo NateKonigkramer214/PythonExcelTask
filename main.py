05d7dd370a7d22c970120e7eccfae6fc957ec470
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-07-25 11:20:32 +1200

    change function names

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas
 
-def  read_excel(filepath, column):
+def  read_csv(filepath, column):
     dataframe = pandas.read_csv(filepath, usecols = column)
     return dataframe
 #Vars
 column = [0, 4]
 filepath = 'D:\Python_Excel_Task\DiamondValues.csv' 
-dataframe = read_excel(filepath, column)
+dataframe = read_csv(filepath, column)
 print(dataframe)
