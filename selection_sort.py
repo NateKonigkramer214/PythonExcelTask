750b2864593a7392b01d0ffc1342bf3e94a94ef1
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-07-25 22:13:07 +1200

    Cleaning code

--- selection_sort.py
+++ selection_sort.py
@@ -1,9 +1,9 @@
 import pandas as pd
-
+#CSV Function
 def read_csv(filepath, columns):
     dataframe = pd.read_csv(filepath, usecols=columns)
     return dataframe
-
+#Sort Function
 def selection_sort(arr):
     n = len(arr)
     for i in range(n - 1):
@@ -22,21 +22,19 @@ def selection_sort(arr):
 columns = [0, 4]
 filepath = 'D:\Python_Excel_Task\DiamondValues.csv'
 dataframe = read_csv(filepath, columns)
-
 # Print the original CSV data
 print("CSV Data: ")
 print(dataframe)
 print("")
-
 # Convert DataFrame to list of dictionaries to perform selection sort
 data_list = dataframe.to_dict('records')
-
+print("Data List: ")
+print(data_list)
 # Sort the data using selection sort based on the 'Price' column
 selection_sort(data_list)
-
+print(" ")
 # Convert the sorted list of dictionaries back to a DataFrame
 sorted_dataframe = pd.DataFrame(data_list)
-
 # Print the sorted DataFrame
 print("Selection Sorted Data:")
 print(sorted_dataframe)
