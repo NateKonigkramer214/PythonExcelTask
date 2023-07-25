2d8c9075d3f7d9a5e8ca80b68b95af697e6978bb
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-07-25 16:48:15 +1200

    Bubble-Sort

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-import pandas
+import pandas as pd
 
-def  read_csv(filepath, column):
-    dataframe = pandas.read_csv(filepath, usecols = column)
+def bubble_sort(item):
+    n = len(item)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if item[j]['Price'] > item[j + 1]['Price']:
+                item[j], item[j + 1] = item[j + 1], item[j]
+
+def read_csv(filepath, columns):
+    dataframe = pd.read_csv(filepath, usecols=columns)
     return dataframe
-#Vars
-column = [0, 4]
-filepath = 'D:\Python_Excel_Task\DiamondValues.csv' 
-dataframe = read_csv(filepath, column)
+
+# Column names you want to read from the CSV file
+columns = [0,4]
+
+# Escaping the backslashes
+filepath = 'D:\Python_Excel_Task\DiamondValues.csv'
+# Read the specified columns from the CSV file into a DataFrame
+dataframe = read_csv(filepath, columns)
+
+# Print the original CSV data
+print("CSV Data: ")
 print(dataframe)
+print("")
+
+# Convert DataFrame to list of dictionaries to perform bubble sort
+data_list = dataframe.to_dict('records')
+
+# Sort the data using bubble sort based on the 'Price' column
+bubble_sort(data_list)
+
+# Convert the sorted list of dictionaries back to a DataFrame
+sorted_dataframe = pd.DataFrame(data_list)
+
+print("Bubble Sorted Data:")
+print(sorted_dataframe)
