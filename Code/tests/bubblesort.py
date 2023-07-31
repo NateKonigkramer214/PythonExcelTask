c8d57ca4e76af0a4df3c077f9bb54e79a6247ea8
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-07-31 16:24:06 +1200

    fix for testing

--- Code/tests/bubblesort.py
+++ Code/tests/bubblesort.py
@@ -1,14 +1,11 @@
 # Python program for implementation of Bubble Sort
-
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
-        # The last i elements are already sorted, so we don't need to check them
-        for j in range(0, n - i - 1):
-            # Swap adjacent elements if they are in the wrong order
+        for j in range(0, n -i -1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
+    return arr 
 
 # Driver code to test above
 item = [64, 34, 25, 12, 22, 11, 90]
