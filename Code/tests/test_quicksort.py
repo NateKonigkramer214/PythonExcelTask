7c5a77351dd76fbf2714fd86e6d712c1a747d50d
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-07-31 15:49:41 +1200

    Test Case Sorting

---
+++ Code/tests/test_quicksort.py
@@ -0,0 +1,29 @@
+"""TODO
+
+-Unsorted 
+-Sorted 
+-Reversed
+-Empty
+
+"""
+import quicksort
+
+def test_unsorted():
+    unsorted_list = [20, 34, 25, 12, 22, 11, 99]
+    assert quicksort.quick_sort(unsorted_list) == [11, 12, 20, 22, 25, 34, 99]
+    
+def test_sorted():
+    sorted_list = [11, 12, 20, 22, 25, 34, 99]
+    assert quicksort.quick_sort(sorted_list) == [11, 12, 20, 22, 25, 34, 99]
+    
+def test_reverse():
+    reverse_list = [99, 34, 25, 22, 20, 12, 11]
+    assert quicksort.quick_sort(reverse_list) == [11, 12, 20, 22, 25, 34, 99]
+    
+def test_empty():
+    empty_list = []
+    assert quicksort.quick_sort(empty_list) == []
+
+def test_duplicate():
+    duplicate_list = [12, 11, 22, 20, 25, 25, 34, 99]
+    assert quicksort.quick_sort(duplicate_list) == [11, 12, 20, 22, 25, 25, 34, 99]
